b05dfc53c0074335a2d869e3da906bd681231745
Ryan Marcos Fragnani <ryanmarcos1708@hotmail.com>
2022-02-08 22:54:31 -0300

    Novos files

--- cardapio.py
+++ cardapio.py
@@ -1,4 +1,7 @@
 class Cardapio:
+  def __init__(self, produto, preço):
+    self.produto = produto
+    self.preço = preço
 
-  def __str__(self):
-    return 'CARDÁPIO\n------------------\nOur Burguers:\n1 - Clássico.....R$16,00\n2 - Frango.......R$16,00\n3 - Siri.........R$18,00\n4 - Costela......R$20,00\n5 - Vegano.......R$18,00\n------------------\nAdicionais:\n1 - Molho.........+(R$2)\n2 - Salada........+(R$4)\n3 - Batata-frita..+(R$5)\n4 - Frango-frito..+(R$8)\n------------------\n'
+  def get_preço(self):
+

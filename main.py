db458c8fd51acecc5b88916715affbbcd59187c9
Zac-Milioli <zacmilioli@gmail.com>
2022-02-17 16:56:14 -0300

    Atualização

--- main.py
+++ main.py
@@ -25,17 +25,5 @@ c = Cardapio()
 
 #### ÁREA DO USUÁRIO ####
 
-c1 = Cliente('Ricardo', 4554, '10')
-c2 = Cliente('Marcos', 3216, '06')
-d = 'Delicioso macarrão, feito com massa caseira, coberto com nosso molho de creme de leite e ovos.'
-p1 = p('Macarrão ao Molho Branco', d, 32)
-d = 'Frango orgânico, com uma crocante crosta, feito em air-fryer, banhado em molho de tomate. Acompanha arroz.'
-p2 = p('Frango à Parmegiana', d, 35)
-c.add_produto(p1)
-c.add_produto(p2)
-
-exibir_cardapio()
-c1.add_pedido(p1)
-get_comanda(c1)
 
 #########################
